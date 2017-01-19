5c0749383489133ae21e49c716759173ccb64ceb
Peter Mogensen <apm@one.com>
2017-01-19 11:32:03 +0100

    Add a dynamic access log handler

---
+++ http/handlers/accesslog/accesslog.go
@@ -0,0 +1,174 @@
+// This file contains slightly modified code from the Gorilla project.
+//
+// Copyright 2013 The Gorilla Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license below
+//
+// Source:
+// https://github.com/gorilla/handlers/blob/master/handlers.go
+//
+// Copyright (c) 2013 The Gorilla Handlers Authors. All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//   Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+// 
+//   Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+package accesslog
+
+import (
+	"io"
+	"net"
+	"net/http"
+
+	"strconv"
+
+	"time"
+	"unicode/utf8"
+
+	"github.com/One-com/gone/http/rrwriter"
+)
+
+const lowerhex = "0123456789abcdef"
+
+func appendQuoted(buf []byte, s string) []byte {
+	var runeTmp [utf8.UTFMax]byte
+	for width := 0; len(s) > 0; s = s[width:] {
+		r := rune(s[0])
+		width = 1
+		if r >= utf8.RuneSelf {
+			r, width = utf8.DecodeRuneInString(s)
+		}
+		if width == 1 && r == utf8.RuneError {
+			buf = append(buf, `\x`...)
+			buf = append(buf, lowerhex[s[0]>>4])
+			buf = append(buf, lowerhex[s[0]&0xF])
+			continue
+		}
+		if r == rune('"') || r == '\\' { // always backslashed
+			buf = append(buf, '\\')
+			buf = append(buf, byte(r))
+			continue
+		}
+		if strconv.IsPrint(r) {
+			n := utf8.EncodeRune(runeTmp[:], r)
+			buf = append(buf, runeTmp[:n]...)
+			continue
+		}
+		switch r {
+		case '\a':
+			buf = append(buf, `\a`...)
+		case '\b':
+			buf = append(buf, `\b`...)
+		case '\f':
+			buf = append(buf, `\f`...)
+		case '\n':
+			buf = append(buf, `\n`...)
+		case '\r':
+			buf = append(buf, `\r`...)
+		case '\t':
+			buf = append(buf, `\t`...)
+		case '\v':
+			buf = append(buf, `\v`...)
+		default:
+			switch {
+			case r < ' ':
+				buf = append(buf, `\x`...)
+				buf = append(buf, lowerhex[s[0]>>4])
+				buf = append(buf, lowerhex[s[0]&0xF])
+			case r > utf8.MaxRune:
+				r = 0xFFFD
+				fallthrough
+			case r < 0x10000:
+				buf = append(buf, `\u`...)
+				for s := 12; s >= 0; s -= 4 {
+					buf = append(buf, lowerhex[r>>uint(s)&0xF])
+				}
+			default:
+				buf = append(buf, `\U`...)
+				for s := 28; s >= 0; s -= 4 {
+					buf = append(buf, lowerhex[r>>uint(s)&0xF])
+				}
+			}
+		}
+	}
+	return buf
+
+}
+
+func preBuildLogLine(buf []byte, req *http.Request, ts time.Time) []byte {
+
+	url := req.URL
+	username := "-"
+	if url.User != nil {
+		if name := url.User.Username(); name != "" {
+			username = name
+		}
+	}
+
+	host, _, err := net.SplitHostPort(req.RemoteAddr)
+
+	if err != nil {
+		host = req.RemoteAddr
+	}
+
+	uri := req.RequestURI
+
+	// Requests using the CONNECT method over HTTP/2.0 must use
+	// the authority field (aka r.Host) to identify the target.
+	// Refer: https://httpwg.github.io/specs/rfc7540.html#CONNECT
+	if req.ProtoMajor == 2 && req.Method == "CONNECT" {
+		uri = req.Host
+	}
+	if uri == "" {
+		uri = url.RequestURI()
+	}
+
+	buf = append(buf, host...)
+	buf = append(buf, " - "...)
+	buf = append(buf, username...)
+	buf = append(buf, " ["...)
+	buf = append(buf, ts.Format("02/Jan/2006:15:04:05 -0700")...)
+	buf = append(buf, `] "`...)
+	buf = append(buf, req.Method...)
+	buf = append(buf, " "...)
+	buf = appendQuoted(buf, uri)
+	buf = append(buf, " "...)
+	buf = append(buf, req.Proto...)
+	buf = append(buf, `" `...)
+
+	return buf
+}
+
+func postBuildLogLine(buf []byte, recorder rrwriter.RecordingResponseWriter) []byte {
+
+	status := recorder.Status()
+	size := recorder.Size()
+	buf = append(buf, strconv.Itoa(status)...)
+	buf = append(buf, " "...)
+	buf = append(buf, strconv.Itoa(size)...)
+	return buf
+}
+
+func writeLog(w io.Writer, buf []byte) error {
+	buf = append(buf, '\n')
+	_, err := w.Write(buf)
+	return err
+}
---
+++ http/handlers/accesslog/loghandler.go
@@ -0,0 +1,150 @@
+package accesslog
+
+import (
+	"sync"
+	"sync/atomic"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/One-com/gone/log"
+	"github.com/One-com/gone/http/rrwriter"
+
+	"unsafe"
+)
+
+var last_reported_accesslog_error atomic.Value
+
+func init() {
+	last_reported_accesslog_error.Store(time.Unix(0, 0))
+}
+
+type buffer [256]byte // temporary byte array for creating log lines.
+
+// DynamicLogHandler is the interface of a logging http.Handler capable
+// of turning on/off access log writing to multiple io.Writer
+type DynamicLogHandler interface {
+	http.Handler
+	// ToggleAccessLog controls which io.Writer accesslog is written to.
+	// call (nil, w) to turn accesslog on a io.Writer on.
+	// call (w, nil) to turn the accesslog of an io.Writer off
+	// call (nil,nil) to turn accesslog completely off.
+	// call (old,new) to atomically which accesslog from one io.Writer to another
+	ToggleAccessLog(old, new io.Writer)
+}
+
+// AuditFunction is a function which will be called by a DynamicLogHandler after a
+// HTTP request has been processed. It can be used for metrics by inspecting the state
+// of the RecordingResponseWriter.
+type AuditFunction func(rrwriter.RecordingResponseWriter)
+
+type logHandler struct {
+	handler http.Handler
+	bufpool *sync.Pool
+	mu      sync.Mutex
+	writers []io.Writer
+	out     unsafe.Pointer // pointer to io.Writer
+	af      AuditFunction
+}
+
+// NewDynamicLogHandler wraps around a provided handler and returns a DynamicLogHandler
+// capable of turning accesslog on/off dynamically.
+// If provided an AuditFunction it will be called after ServeHTTP on wrapped handler
+// returns
+func NewDynamicLogHandler(h http.Handler, af AuditFunction) DynamicLogHandler {
+	p := &sync.Pool{New: func() interface{} { return new(buffer) }}
+	return &logHandler{handler: h, bufpool: p, af: af}
+}
+
+func (h *logHandler) ToggleAccessLog(old, new io.Writer) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	if old == nil { // Enable logging for new io.Writer
+		h.writers = append(h.writers, new)
+
+		out := io.MultiWriter(h.writers...)
+		
+		//h.out.Store(out)
+		atomic.StorePointer(&h.out, unsafe.Pointer(&out))
+		return
+	}
+
+	// walk through the writers, find the one needing to be retired.
+	var i,j int
+	for i < len(h.writers) {
+		wo := h.writers[i]
+		var wn io.Writer
+		if wo == old {
+			if new == nil { // remove the log writer
+				i++
+				continue
+			}
+			wn = new // put in the new
+			
+		} else {
+			// keep the old
+			wn = h.writers[i]
+		}
+		h.writers[j] = wn
+		j++
+		i++
+	}
+	h.writers = h.writers[:j]
+
+	if len(h.writers) == 0 {
+		atomic.StorePointer(&h.out, unsafe.Pointer(nil))
+		//h.out.Store(nil)
+	} else {
+		out := io.MultiWriter(h.writers...)
+		atomic.StorePointer(&h.out, unsafe.Pointer(&out))
+		//h.out.Store(out)
+	}
+}
+
+func (h *logHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+
+	//out := h.out.Load()
+
+	var out unsafe.Pointer
+	out = atomic.LoadPointer(&h.out)
+	
+	if out != nil {
+
+		outw := *((*io.Writer)(out))
+		
+		recorder := rrwriter.MakeRecorder(w)
+	
+		t := time.Now()
+		pbuf := h.bufpool.Get().(*buffer)
+		logbuf := pbuf[:0]
+
+		logbuf = preBuildLogLine(logbuf, req, t)
+		h.handler.ServeHTTP(recorder, req)
+		logbuf = postBuildLogLine(logbuf, recorder)
+
+		err := writeLog(outw, logbuf)
+		if err != nil {
+			last := last_reported_accesslog_error.Load().(time.Time)
+			if t.Sub(last) > time.Minute {
+				log.CRIT("Error writing access log, supressing for a minute", "err", err)
+				last_reported_accesslog_error.Store(t)
+			}
+		}
+		h.bufpool.Put(pbuf)
+
+		if h.af != nil {
+			h.af(recorder)
+		}
+		
+	} else {
+		if h.af != nil {
+			recorder := rrwriter.MakeRecorder(w)			
+			h.handler.ServeHTTP(recorder, req)
+			h.af(recorder)
+		} else {
+			h.handler.ServeHTTP(w, req)
+		}
+	}
+}
+
